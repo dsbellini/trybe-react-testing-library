8e260c7962f6ff10947691e18806adba4c8a0dc3
Danilo Bellini <bellini.daan@gmail.com>
2023-06-27 17:46:58 -0300

    Requisito 3 ok

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+import pokemonList from '../data';
+
+test('Testa se não tem nenhum poke favorito', () => {
+  renderWithRouter(<FavoritePokemon pokemonList={ [] } />);
+
+  const noFavoritePokemon = screen.getByText(/no favorite pokémon found/i);
+  expect(noFavoritePokemon).toBeInTheDocument();
+});
+
+test('Testa os pokes favoritos', async () => {
+  const favoritesPokemons = [pokemonList[0], pokemonList[1]];
+
+  renderWithRouter(<FavoritePokemon pokemonList={ favoritesPokemons } />);
+
+  favoritesPokemons.forEach((pokemon) => {
+    const pokemonName = screen.getByText(pokemon.name);
+    expect(pokemonName).toBeInTheDocument();
+  });
+});
