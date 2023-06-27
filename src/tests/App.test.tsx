7613bb10efa43983c6bb33200b2f955206a06bfb
Danilo Bellini <bellini.daan@gmail.com>
2023-06-27 15:49:19 -0300

    Initial commit & Requisito 1

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Testa a barra de navegação e seu conteúdo (Home, About, Favorite Poke)', () => {
+  renderWithRouter(<App />);
+  const home = screen.getByRole('link', { name: /home/i });
+  const about = screen.getByRole('link', { name: /about/i });
+  const favoritePokemon = screen.getByRole('link', { name: /favorite pokémon/i });
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favoritePokemon).toBeInTheDocument();
+});
